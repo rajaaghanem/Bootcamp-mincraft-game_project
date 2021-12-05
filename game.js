eebbf494474847d4d1ef4435b6f216ab5b33c7fa
rajaaghanem <rajaaghanem@gmail.com>
2021-12-05 23:33:43 +0200

    board file

--- game.js
+++ game.js
@@ -1,14 +1,13 @@
+import { matrix } from "/game_board-first.js";
 import { matrixSecound } from "/game_board-secound.js";
 import { matrixDark } from "/dark_mode-board.js";
-import { pickaxe, shovel, axe} from "/tools.js";
-import {inventory, inventoryClasses, resetInventory} from "/inventory.js";
+import { pickaxe, shovel, axe } from "/tools.js";
+import { inventory, inventoryClasses, resetInventory } from "/inventory.js";
 
 const startButton = document.querySelector(".start-btn");
 const landingPage = document.querySelector(".landing-page");
-const activeClass = document.querySelector(".active-btn");
 const resetButton = document.querySelector(".reset-btn");
 export const gameBoard = document.querySelector(".game-board");
-const firstBoard = document.querySelector('[data-board="first-board"]');
 
 export let theGame = {
   selectedTool: "",
@@ -27,42 +26,10 @@ export let colors = {
   packs: "packs",
   stones: "stones",
   darkPacks: "dark-packs",
-  darkGreen : "dark-green",
-  darkBrownMode : "dark-mode-brwon",
-  darkOak:"dark-oak"
+  darkGreen: "dark-green",
+  darkBrownMode: "dark-mode-brwon",
+  darkOak: "dark-oak",
 };
-
-let matrix = [
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 5, 5, 5, 5, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 5, 5, 5, 5, 5, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 5, 5, 5, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0],
-  [0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0],
-  [7, 7, 0, 2, 2, 2, 0, 0, 0, 7, 7, 0, 0, 0, 1, 1, 0, 3, 0, 0, 1],
-  [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6], 
-  [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
-  [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
-  [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
-  [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
-  [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
-];
-
-firstBoard.addEventListener("click", (event) => {
-  theGame.board = "first";
-  gameBoard.style.backgroundColor = "rgb(89, 177, 218)";
-  resetGame(matrix);
-  CreatingTheBoard(matrix);
-});
 
 //starting the game
 startButton.addEventListener("click", (event) => {
@@ -118,7 +85,7 @@ export function CreatingTheBoard(matrixEl) {
 }
 
 // add the element to the game-board
-export function addElement(gameElement, i, j) {
+function addElement(gameElement, i, j) {
   gameElement.style.gridRowStart = i;
   gameElement.style.gridColumnStart = j;
   gameBoard.appendChild(gameElement);
@@ -126,60 +93,53 @@ export function addElement(gameElement, i, j) {
 
 //clicking on the gameboard
 gameBoard.addEventListener("click", (e) => {
-
   if (theGame.clickedOnInventory && !theGame.isEmptyInventory) {
-      e.target.classList = "";
-      e.target.classList.value = inventory.classList.value;
-      console.dir(inventory.classList);
-      resetInventory();
-    }
-
-  else {
+    e.target.classList = "";
+    e.target.classList.value = inventory.classList.value;
+    console.dir(inventory.classList);
+    resetInventory();
+  } else {
     switch (theGame.selectedTool) {
-    case "pickaxe":
-      if (
-        e.target.classList.value === colors.gray ||
-        e.target.classList.value === colors.stones 
-      ) {
-        inventoryClasses(e.target.classList.value);
-        console.dir(inventory.classList);
-        pickaxe.classList.remove("unactive-btn");
-        e.target.classList = "";
-      } else pickaxe.classList.add("unactive-btn");
-      break;
+      case "pickaxe":
+        if (
+          e.target.classList.value === colors.gray ||
+          e.target.classList.value === colors.stones
+        ) {
+          inventoryClasses(e.target.classList.value);
+          console.dir(inventory.classList);
+          pickaxe.classList.remove("unactive-btn");
+          e.target.classList = "";
+        } else pickaxe.classList.add("unactive-btn");
+        break;
 
-    case "shovel":
-      if (
-        e.target.classList.value === colors.brown ||
-        e.target.classList.value === colors.packs 
-        || e.target.classList.value === colors.darkPacks || e.target.classList.value === colors.darkBrownMode
+      case "shovel":
+        if (
+          e.target.classList.value === colors.brown ||
+          e.target.classList.value === colors.packs ||
+          e.target.classList.value === colors.darkPacks ||
+          e.target.classList.value === colors.darkBrownMode
+        ) {
+          inventoryClasses(e.target.classList.value);
+          shovel.classList.remove("unactive-btn");
+          e.target.classList = "";
+        } else shovel.classList.add("unactive-btn");
+        break;
 
-      ) {
-        inventoryClasses(e.target.classList.value);
-        shovel.classList.remove("unactive-btn");
-        e.target.classList = "";
-      } else shovel.classList.add("unactive-btn");
-      break;
-
-    case "axe":
-      if (
-        e.target.classList.value === colors.darkbrown ||
-        e.target.classList.value === colors.green ||
-        e.target.classList.value === colors.darkGreen||
-        e.target.classList.value === colors.darkOak
-
-
-      ) {
-        inventoryClasses(e.target.classList.value);
-        axe.classList.remove("unactive-btn");
-        e.target.classList = "";
-      } else axe.classList.add("unactive-btn");
-      break;
+      case "axe":
+        if (
+          e.target.classList.value === colors.darkbrown ||
+          e.target.classList.value === colors.green ||
+          e.target.classList.value === colors.darkGreen ||
+          e.target.classList.value === colors.darkOak
+        ) {
+          inventoryClasses(e.target.classList.value);
+          axe.classList.remove("unactive-btn");
+          e.target.classList = "";
+        } else axe.classList.add("unactive-btn");
+        break;
+    }
   }
-}
-
 });
-
 
 //reset the game-board
 resetButton.addEventListener("click", (event) => {
